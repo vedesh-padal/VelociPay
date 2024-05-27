1b6004197a7881b34afc3016cf10d8cb236c55ae
Vedesh Padal <vpadal04@gmail.com>
2024-05-27 16:35:38 +0530

    prisma client init done

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,4 +1,7 @@
 import { Button } from "@repo/ui/button";
+import { PrismaClient } from "@repo/db/client";
+
+const client = new PrismaClient();
 
 export default function Page(): JSX.Element {
   return (
